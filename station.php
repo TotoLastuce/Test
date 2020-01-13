375974a9ea7f15959bbe03b0b69187da891f216e
TotoLastuce <scherrer.thomas@hotmail.fr>
2020-01-13 16:55:21 +0100

    ajout du menu, de la page stations.php et lien hypertexte

---
+++ station.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="stylegit.css">
+    <link href="https://fonts.googleapis.com/css?family=Righteous&display=swap" rel="stylesheet">
+    <title>Agence de voyage</title>
+</head>
+
+<body>
+    <?php
+    /****************************************************************************
+    Nom du script   :   station.php
+    Description     :   Script qui se connecte au serveur de base de données et à la BD                     "agence_de_voyage" pour interroger la table "station" et                             afficher les résultats dans un tableau html
+    Auteur          :   Thomas
+    Version         :   1.0
+    Date            :   13/01/2020
+    ****************************************************************************/
+    ?>
+    <h1>Les stations</h1>
+</body>
+</html>
